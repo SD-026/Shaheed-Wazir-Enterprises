eaac98ab0a6552b2526945b22f07b0f5197200ee
SD-026 <140894438+SD-026@users.noreply.github.com>
2025-04-30 04:20:02 +0500

    api2

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,12 +3,12 @@ import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 export const createUser = async (req, res) => {
-    const { name, username, email, password } = req.body;
+    const {  username, email, password } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-        name, username, email, password: hashedPassword,
+         username, email, password: hashedPassword,
     });
 
     res.status(201).json(user);
