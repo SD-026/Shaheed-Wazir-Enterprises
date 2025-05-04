89ea6d4319428f40a200600751b0d0819bc561a5
SD-026 <140894438+SD-026@users.noreply.github.com>
2025-05-04 12:30:14 +0500

    updatre7

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import LoginPage from '../src/pages/user/LoginPage';
+import LoginPage from '../src/pages/user/LoginPage.jsx';
 import Dashboard from './pages/Admin/Dashboard';
-import UserDashboard from '../src/pages/user/UserDashboard';
+import UserDashboard from '../src/pages/user/UserDashboard.jsx';
 import CustomerManagement from './pages/Admin/CustomerManagement';
-import AddCustomerForm from '../src/pages/user/AddCustomerForm';
+import AddCustomerForm from '../src/pages/user/AddCustomerForm.jsx';
 import TransactionManagement from './pages/Admin/TransactionManagement';
 import AddTransactionForm from './pages/Admin/AddTransactionForm';
 import CustomerTransactions from './pages/Admin/CustomerTransactions';
